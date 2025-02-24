dead8e711ed2c8caf364dd1ee6effda54118103d
nicoluvas <nicolaspoficial@gmail.com>
2025-02-24 10:42:44 -0300

    +user settings

--- src/userplugins/confirmAlert/index.tsx
+++ src/userplugins/confirmAlert/index.tsx
@@ -2,24 +2,24 @@ import { Devs } from "@utils/constants";
 import { definePluginSettings } from "@api/Settings";
 import definePlugin, { OptionType } from "@utils/types";
 import { ModalRoot, ModalProps, ModalHeader, ModalFooter } from "@utils/modal";
-import { Forms } from "@webpack/common";
 
 
-export function confirmationModal(mode:string, { rootProps }: { rootProps: ModalProps; }) {
-    return (
-        <ModalRoot {...rootProps}>
 
-            <ModalHeader>
-                <Forms.FormTitle tag="h2">
-                    Are you sure?
-                </Forms.FormTitle>
-            </ModalHeader>
+export function confirmationModal({ rootProps }: { rootProps: ModalProps; }) {
+  return (
+    <ModalRoot {...rootProps}>
 
-            <ModalFooter>
-                <p>Are you sure you want to {mode}?</p>
-            </ModalFooter>
-        </ModalRoot>
-    )
+      <ModalHeader>
+
+      </ModalHeader>
+
+      <ModalFooter>
+        <p>Are you sure you want to {settings.store.behavior}?</p>
+        <p>Are you sure you want to {settings.store.defaultOption ? "YES" : "NO"}?</p>
+      </ModalFooter>
+
+    </ModalRoot>
+  );
 }
 
 
@@ -33,6 +33,11 @@ const settings = definePluginSettings({
       { label: "Restart", value: "restart" },
     ],
   },
+  defaultOption: {
+    type: OptionType.BOOLEAN,
+    description: "Define YES as an option as the default one",
+    default: true
+  }
 });
 
 export default definePlugin({
@@ -40,4 +45,5 @@ export default definePlugin({
   description:
     "Prevent accidental quits (CTRL + Q) or restarts (CTRL + R) by showing a confirmation dialog",
   authors: [Devs.mura],
+  settings,
 });
